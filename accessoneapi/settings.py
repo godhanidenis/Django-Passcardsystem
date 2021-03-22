fd95496bf0a4ce394537a39908cf134e98c466f4
godhanidenis <40867528+godhanidenis@users.noreply.github.com>
2021-03-22 12:49:01 +0530

    Updated settings.py

--- accessoneapi/settings.py
+++ accessoneapi/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -147,13 +148,20 @@ USE_TZ = True
 
 # STATIC_URL = '/static/'
 
+# STATIC_URL = '/static/'
+# # STATIC_ROOT = '/static'
+# STATIC_ROOT = '/home/dev/accessone-api/static'
+
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/static'
-STATIC_ROOT = '/home/dev/accessone-api/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = '/media'
-MEDIA_ROOT = '/home/dev/accessone-api/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# MEDIA_URL = '/media/'
+# # MEDIA_ROOT = '/media'
+# MEDIA_ROOT = '/home/dev/accessone-api/media'
 
 django_heroku.settings(locals())
